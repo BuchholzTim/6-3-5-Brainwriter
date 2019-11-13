a0d7d33c34aa49a614c7b9a6b7c47be176681c16
Marc Bollmann <m4rc9696@live.de>
2019-11-13 14:23:54 +0100

    Test Commit New Branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Hallo Test</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
