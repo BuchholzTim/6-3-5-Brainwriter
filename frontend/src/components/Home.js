f69d6c2e4aca921bcf2c5f7356a1133122d7e42b
Marc Bollmann <m4rc9696@live.de>
2019-11-23 05:08:52 +0100

    Visually improved Home-Component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,46 +1,77 @@
-import React, {Component} from "react";
-import {Button, Box} from 'grommet';
+import React, { Component } from "react";
+import { Button, Box } from "grommet";
+import { Group, Login} from "grommet-icons"
 import Topic from "./topic/Topic";
 import JoinRound from "./JoinRound";
-import {socket} from "../socket/socket";
+import { socket } from "../socket/socket";
 
 export class Home extends Component {
-    state = {
-        page: ""
-    };
+  state = {
+    page: ""
+  };
 
-    switchPage = event => {
-        this.setState({
-            page: event.target.value
+  switchPage = event => {
+    this.setState({
+      page: event.target.value
+    });
+  };
+
+  renderPage(page) {
+    switch (page) {
+      case "1":
+        socket.emit("test");
+        return <Topic />;
+      case "2":
+        socket.emit("joinTopic", {
+          joinCode: "testRoom"
         });
-    };
+        return <JoinRound />;
+      default:
+        return (
+          <Box
+            direction="row-responsive"
+            justify="center"
+            align="center"
+            pad="xlarge"
+            gap="medium"
+          >
+            <Box
+              pad="large"
+              align="center"
+              background={{ color: "light-6", opacity: "strong" }}
+              round
+              gap="small"
+            >
+              <Group size="large" color="dark-2" />
+              <Button primary label="Schnelle Runde erstellen" value="1" onClick={this.switchPage} />
+            </Box>
 
-    renderPage(page) {
-        switch (page) {
-            case "1":
-                socket.emit("test");
-                return <Topic/>;
-            case "2":
-                socket.emit("joinTopic", {
-                    joinCode: "testRoom"
-                });
-                return <JoinRound/>;
-            default:
-                return (
-                    <Box direction="column">
+            <Box
+              pad="large"
+              align="center"
+              background="light-6"
+              round
+              gap="small"
+            >
+              <Login size="large" color="dark-2" />
+              <Button primary label="Runde beitreten" value="2" onClick={this.switchPage} />
+            </Box>
+          </Box>
+
+          /*<Box direction="column">
                         <Button color="brand" label="Schnelle Runde erstellen" value="1" onClick={this.switchPage}/>
                         <br/>
                         <Button label="Runde beitreten" value="2" onClick={this.switchPage}/>
                         <br/>
-                    </Box>
-                );
-        }
+                    </Box>*/
+        );
     }
+  }
 
-    render() {
-        const {page} = this.state;
-        return this.renderPage(page);
-    }
+  render() {
+    const { page } = this.state;
+    return this.renderPage(page);
+  }
 }
 
 export default Home;
