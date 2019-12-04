661bc346de3c267d72622f90958de07ccf667ff5
Marc Bollmann <m4rc9696@live.de>
2019-12-04 14:59:22 +0100

    Added Github Icon with Link to Menubar component

--- frontend/src/components/Menubar.js
+++ frontend/src/components/Menubar.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 import { Box, Heading } from "grommet";
+import { Github } from "grommet-icons";
 
 export class Menubar extends Component {
+  goToGithub = () => {
+    window.open("https://github.com/BuchholzTim/6-3-5-Brainwriter", "_blank");
+  };
+
   render() {
     return (
       <Box
         as="header"
-        direction="row-reverse"
-        alignSelf="center"
+        direction="row"
+        align="center"
+        justify="between"
         pad={{ vertical: "xsmall", horizontal: "xsmall" }}
         background="brand"
         elevation="large"
+        gap="small"
         style={{ zIndex: "1000" }}
       >
         <Heading level={3} margin="none" color="white" align="end">
           6-3-5 Brainwriter
         </Heading>
+        <Github
+          onClick={this.goToGithub}
+          size="large"
+          color="white"
+          alignmentBaseline="center"
+        ></Github>
       </Box>
     );
   }
