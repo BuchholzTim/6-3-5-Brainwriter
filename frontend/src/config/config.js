57c6aae9f711685a9b3d0113406048d49a55be8a
Tim Buchholz <buchholz.tim@gmx.de>
2019-12-05 17:56:47 +0100

    Added config Switch for Production Build

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,3 +1,4 @@
 // export const backend_ip = "http://192.168.2.102:";
 export const backend_ip = "http://localhost:";
 export const backend_port = "3001";
+export const production_build = true;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,16 +1,24 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { production_build } from "../config/config";
 
 const initialState = {};
 const middleware = [thunk];
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = production_build
+  ? createStore(
+      rootReducer,
+      initialState,
+      compose(applyMiddleware(...middleware))
+    )
+  : createStore(
+      rootReducer,
+      initialState,
+      compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    );
 
 export default store;
