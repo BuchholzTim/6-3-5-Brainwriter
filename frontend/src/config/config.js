689fb62f4c934594c4a0964a2425ba4ea737a516
Tim Buchholz <buchholz.tim@gmx.de>
2020-01-13 22:58:24 +0100

    Small Change to react-script

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,4 @@
-// export const backend_ip = "http://192.168.2.102:";
 export const backend_ip = "http://localhost:";
 export const backend_port = "3001";
+// If set to true, the Dev-Extensions for Redux are enabled!
 export const production_build = true;
