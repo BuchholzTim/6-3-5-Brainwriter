6b344964bdf29d2ecb7071c3971e21002f932cbc
Marc Bollmann <m4rc9696@live.de>
2020-06-01 16:22:14 +0200

    Backend Update for Topics implemented #7

--- backend/routes/topicsRoute.js
+++ backend/routes/topicsRoute.js
@@ -32,6 +32,56 @@ router.post("/create", async function (req, res, next) {
   models.Topic.create(Topic).then((Topic) => res.json({ topic: Topic }));
 });
 
+router.post("/update", async function (req, res) {
+  const joinCode = req.body.joinCode;
+  const topic = req.body.topic;
+  const timePerRound = req.body.timePerRound;
+  const active = req.body.active;
+
+  // Get the topic with the specific joinCode
+  let existingTopic = await getTopic(joinCode);
+
+  // Check if the topic with the joinCode really exists
+  if (existingTopic) {
+    // Check which params are set in the Request and replace them with the old ones
+    existingTopic.joinCode = joinCode;
+    if (topic != null) {
+      existingTopic.topic = topic;
+    }
+
+    if (timePerRound != null) {
+      existingTopic.timePerRound = timePerRound;
+    }
+
+    if (active != null) {
+      existingTopic.active = active;
+    }
+
+    const Topic = {
+      topic: existingTopic.topic,
+      joinCode: existingTopic.joinCode,
+      timePerRound: existingTopic.timePerRound,
+      active: existingTopic.active,
+    };
+
+    // Update Topic in the DB with the corresponding joinCode
+    models.Topic.update(Topic, { where: { joinCode: Topic.joinCode } }).then(
+      (Topic) =>
+        res.json({
+          joinCode: existingTopic.joinCode,
+          topic: existingTopic.topic,
+          timePerRound: existingTopic.timePerRound,
+          active: existingTopic.active,
+        })
+    );
+  } else {
+    res
+      .status(404)
+      .send('Topic with "joinCode": ' + '"' + joinCode + '" Not Found');
+    res.end();
+  }
+});
+
 router.get("/get", async function (req, res, next) {
   const joinCode = req.query.joinCode;
   const existingTopic = await getTopic(joinCode);
@@ -58,6 +108,12 @@ router.post("/join", async function (req, res, next) {
     res.status(400).send("Topic is undefined");
     res.end();
     return;
+  }
+
+  // If topic is not active
+  if (!existingTopic.active) {
+    res.status(400).send("Topic is not active anymore");
+    res.end();
   }
 
   const Author = {
