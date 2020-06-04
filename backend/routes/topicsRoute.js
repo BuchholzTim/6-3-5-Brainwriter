BuchholzTim/6-3-5-Brainwriter

commit 8b3cf69bed509aee75a34b69da266f768384df02

"Null"-Check now uses the checkExistence function

backend/routes/topicsRoute.js
@@ -45,15 +45,15 @@ router.post("/update", async function (req, res) {
   if (existingTopic) {
     // Check which params are set in the Request and replace them with the old ones
     existingTopic.joinCode = joinCode;
-    if (topic != null) {
+    if (tools.checkExistence(topic)) {
       existingTopic.topic = topic;
     }
 
-    if (timePerRound != null) {
+    if (tools.timePerRound(topic)) {
       existingTopic.timePerRound = timePerRound;
     }
 
-    if (active != null) {
+    if (tools.timePerRound(active)) {
       existingTopic.active = active;
     }
 

commit d71a714afdfb2b9de76b6a35c18b5e0948abe4d5

Fixed Bug introduced in 8b3cf69bed509aee75a34b69da266f768384df02

backend/routes/topicsRoute.js
@@ -49,11 +49,11 @@ router.post("/update", async function (req, res) {
       existingTopic.topic = topic;
     }
 
-    if (tools.timePerRound(topic)) {
+    if (tools.checkExistence(timePerRound)) {
       existingTopic.timePerRound = timePerRound;
     }
 
-    if (tools.timePerRound(active)) {
+    if (tools.checkExistence(active)) {
       existingTopic.active = active;
     }
 