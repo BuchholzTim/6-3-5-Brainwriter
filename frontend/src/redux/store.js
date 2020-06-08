f96d928a0f6bd9268785c09cb993e4153fd083a0
Tim Buchholz <buchholz.tim@gmx.de>
2020-06-08 14:43:20 +0200

    Added Support for Remote-DevTools

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,15 +1,27 @@
 import { convertStringToBoolean } from "../tools/tools";
 import { createStore, applyMiddleware, compose } from "redux";
+import devToolsEnhancer, { composeWithDevTools } from "remote-redux-devtools";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
-const middleware = [thunk];
+const middleware = [thunk]; //
+/* If Production-Build is set to true don't use Redux-Dev-Tools
+ * If it is set to true -> Check if Safari is used.
+ * Safari doesn't support the native Dev-Tools so we use the remote-devtools on: http://remotedev.io/local
+ * If Safari is set to false -> Use Dev-Tool-Extension: http://extension.remotedev.io/#installation
+ */
 const store = convertStringToBoolean(process.env.REACT_APP_PRODUCTION_BUILD)
   ? createStore(
       rootReducer,
       initialState,
       compose(applyMiddleware(...middleware))
+    )
+  : convertStringToBoolean(process.env.REACT_APP_SAFARI)
+  ? createStore(
+      rootReducer,
+      initialState,
+      composeWithDevTools(applyMiddleware(...middleware))
     )
   : createStore(
       rootReducer,
