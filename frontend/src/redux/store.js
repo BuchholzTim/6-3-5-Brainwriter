84a9467f2f586ac168bd8dc7bad899534ee7c8fd
Tim Buchholz <buchholz.tim@gmx.de>
2020-06-12 13:04:45 +0200

    Removed Ternary Operator to If-Else for Clarity

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,6 @@
 import { convertStringToBoolean } from "../tools/tools";
 import { createStore, applyMiddleware, compose } from "redux";
-import devToolsEnhancer, { composeWithDevTools } from "remote-redux-devtools";
+import { composeWithDevTools } from "remote-redux-devtools";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
@@ -11,26 +11,29 @@ const middleware = [thunk]; //
  * Safari doesn't support the native Dev-Tools so we use the remote-devtools on: http://remotedev.io/local
  * If Safari is set to false -> Use Dev-Tool-Extension: http://extension.remotedev.io/#installation
  */
-const store = convertStringToBoolean(process.env.REACT_APP_PRODUCTION_BUILD)
-  ? createStore(
-      rootReducer,
-      initialState,
-      compose(applyMiddleware(...middleware))
-    )
-  : convertStringToBoolean(process.env.REACT_APP_SAFARI)
-  ? createStore(
-      rootReducer,
-      initialState,
-      composeWithDevTools(applyMiddleware(...middleware))
-    )
-  : createStore(
-      rootReducer,
-      initialState,
-      compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
-    );
+let store;
 
+if (convertStringToBoolean(process.env.REACT_APP_PRODUCTION_BUILD)) {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
+} else if (convertStringToBoolean(process.env.REACT_APP_SAFARI)) {
+  store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+}
 export default store;
