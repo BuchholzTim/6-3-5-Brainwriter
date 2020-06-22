6eb43b808e0291828543d7ff53e6fafce0f68f4a
Marc Bollmann <m4rc9696@live.de>
2020-06-22 13:47:00 +0200

    Added topics/get and topics/join-route #27

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,8 @@ const app = express();
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerDefinition = {
+  components: {},
+  openapi: "3.0.0",
   info: {
     title: "Brainwriter Backend",
     description: "Brainwriter API Information",
