d7f1d9e46894d68b15e259cf3ae0d7f65a273e49
Armin <40383183+Preefix@users.noreply.github.com>
2020-07-13 15:38:22 +0200

    generated hidden content which is not visible to the user, added 'export all'button to export all tables at once and not only the ones which are rendered; TODO: styling

--- frontend/src/components/tools/Summary.js
+++ frontend/src/components/tools/Summary.js
@@ -3,12 +3,13 @@ import { connect } from "react-redux";
 import { Box, Button } from "grommet";
 import IdeaTable from "../playerView/subPages/ideaComponents/IdeaTable";
 import QuestionBox from "./QuestionBox";
-import { FormUpload } from 'grommet-icons'
+import { DocumentPdf } from 'grommet-icons'
 import { PDFExport } from '@progress/kendo-react-pdf';
 
 export class PlayerViewSummary extends Component {
 
   pdfExportComponent;
+  pdfExportAllComponent;
 
   state = {
     shownTable: 0
@@ -26,6 +27,10 @@ export class PlayerViewSummary extends Component {
 
   exportPDFWithComponent = () => {
     this.pdfExportComponent.save();
+  }
+
+  exportALLWithComponent = () => {
+    this.pdfExportAllComponent.save();
   }
 
   render() {
@@ -46,43 +51,38 @@ export class PlayerViewSummary extends Component {
 
     return (
       <div>
-        <PDFExport 
-          ref={(component) => this.pdfExportComponent = component} 
-          paperSize="A4"
-          fileName={ `${ topic }` }
-          >
-          <div>
-            <Box
-              style={{ wordWrap: "break-word" }}
-              direction="column"
-              gap="medium"
-              pad="small"
-              overflow={{ horizontal: "auto" }}
-            >
-              <QuestionBox question={topic} />
-              {tables[shownTable]}
-            </Box>
-          </div>
-        </PDFExport>
+        <div>
           <Box
-           direction="column"
-           align="center"
-           gap="medium"
-           pad="small"
-           overflow={{
-             horizontal: "auto" 
-            }}
+            style={{ wordWrap: "break-word" }}
+            direction="column"
+            gap="medium"
+            pad="small"
+            overflow={{ horizontal: "auto" }}
           >
-          <Button
-                primary
-                hoverIndicator="true"
-                style={{ width: "100%" }}
-                onClick={this.showNextTable}
-                label="Next"
-              />
+            <QuestionBox question={topic} />
+            {tables[shownTable]}
+          </Box>
+        </div>
+
+        <Box
+          direction="column"
+          align="center"
+          gap="medium"
+          pad="small"
+          overflow={{
+            horizontal: "auto"
+          }}
+        >
           <Button
             primary
-            icon={<FormUpload color="white" />}
+            hoverIndicator="true"
+            style={{ width: "100%" }}
+            onClick={this.showNextTable}
+            label="Next"
+          />
+          <Button
+            primary
+            icon={<DocumentPdf color="white" />}
             style={
               {
                 width: "30%",
@@ -92,9 +92,68 @@ export class PlayerViewSummary extends Component {
             onClick={this.exportPDFWithComponent}
             label="Export PDF"
           />
-          </Box>
-          
-       
+
+          <Button
+            primary
+            icon={<DocumentPdf color="white" />}
+            style={
+              {
+                width: "30%",
+                background: "red"
+              }
+            }
+            onClick={this.exportALLWithComponent}
+            label="Export all"
+          />
+        </Box>
+
+        {/* 
+        This is a hacky solution: 
+        In order to export a custom pdf file 
+        which has content the user should not see on screen 
+        we have to do it that way 
+        */}
+        <div style={{ position: "absolute", left: "-1000px", top: 0, width: "500px" }}>
+          <PDFExport
+            ref={(component) => this.pdfExportComponent = component}
+            paperSize="A4"
+            fileName={`${topic}`}
+          >
+            <Box
+              style={{
+                wordWrap: "break-word"
+              }}
+              direction="column"
+              gap="medium"
+              pad="small"
+              overflow={{ horizontal: "auto" }}
+            >
+              <QuestionBox question={topic} />
+              {tables[shownTable]}
+            </Box>
+          </PDFExport>
+        </div>
+
+        <div style={{ position: "absolute", left: "-1000px", top: 0, width: "500px" }}>
+          <PDFExport
+            ref={(component) => this.pdfExportAllComponent = component}
+            paperSize="A4"
+            fileName={`${topic}`}
+          >
+            <Box
+              style={{
+                wordWrap: "break-word"
+              }}
+              direction="column"
+              gap="medium"
+              pad="small"
+              overflow={{ horizontal: "auto" }}
+            >
+              <QuestionBox question={topic} />
+              {tables}
+            </Box>
+          </PDFExport>
+        </div>
       </div>
     );
   }
